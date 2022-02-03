875410cd51a155f321596252f82b1814d9adc0cb
Jake Finn <lof.steven@gmail.com>
2022-02-03 13:34:25 -0600

    Add logic to video.js

---
+++ public/js/video.js
@@ -0,0 +1,11 @@
+let camera = document.querySelector("#camera");
+
+if (navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+      .then(function (stream) {
+        video.srcObject = stream;
+      })
+      .catch(function (err0r) {
+        console.log("Error");
+      });
+  }
