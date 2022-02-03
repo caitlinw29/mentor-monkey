0c935718140a7b599f0d89ba74d29fedcb7d94bc
Stephanie Fajardo <stfajardo@gmail.com>
2022-02-03 18:12:36 -0500

    write a GET route for users database

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,9 +2,29 @@ const router = require('express').Router();
 const { User } = require('../../models');
 
 //signup (create user)
+// this route is for /api/users
+
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.create(req.body);
+    console.log(userData);
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 router.post('/', async (req, res) => {
   try {
-    
+    const userData = await User.create(req.body); 
+
+    req.session.save(() => {
+      req.session.email = userData.email; 
+      req.session.password = userData.password;
+
+      res.status(200).json(userData);
+    });
+
   } catch (err) {
     res.status(400).json(err);
   }
