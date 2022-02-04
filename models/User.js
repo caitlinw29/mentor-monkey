3867d4859e19d90ba3bf7f204b72402fc89cdd04
Caitlin Wall <caitlinwall96@gmail.com>
2022-02-04 15:35:20 -0500

    Add bcrypt to User model for checkPassword

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 
 const { Model, DataTypes } = require('sequelize');
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
@@ -34,16 +35,6 @@ User.init(
     },
   },
   {
-    // hooks: {
-    //   beforeCreate: async (newUserData) => {
-    //     newUserData.password = await bcrypt.hash(newUserData.password, 8);
-    //     return newUserData;
-    //   },
-    //   beforeUpdate: async (updatedUserData) => {
-    //     updatedUserData.password = await bcrypt.hash(updatedUserData.password, 8);
-    //     return updatedUserData;
-    //   },
-    // },
     sequelize,
     timestamps: false,
     freezeTableName: true,
