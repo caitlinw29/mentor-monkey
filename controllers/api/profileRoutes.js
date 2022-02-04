2e7b8a78cee645de23902734c06ecf87af1636af
ealisa818 <ealisa818@gmail.com>
2022-02-04 09:45:49 -0500

    complete profile_form

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -2,15 +2,34 @@ const router = require('express').Router();
 const { Profile } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-//create profile
+// create profile
 router.post('/', withAuth, async (req, res) => {
   try {
-   
+    const newProfile = await Profile.create({
+      name: req.body.name,
+      about: req.body.about,
+      languages: req.body.languages,
+    });
+    res.status(200).json(newProfile);
   } catch (err) {
-    res.status(400).json(err);
+     res.status(400).json(err);
   }
 });
 
+    // Send the newly created row as a JSON object
+
 //update profile with put?
+router.put('/:id', withAuth, async (req, res) => {
+  try {
+    const profileData = await Profile.update({
+      ...req.user_id,
+      user_id: req.session.user_id,
+    });
+    res.status(200).json(profileData);
+  } catch (err) {
+    res.status(400).json(err)
+  }
+
+});
 
 module.exports = router;
