3aa87f79881dee0d042d031697559df0c91037ce
Caitlin Wall <caitlinwall96@gmail.com>
2022-02-04 11:07:28 -0500

    Add login route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -40,7 +40,7 @@ router.get('/signup', async (req, res) => {
 
 router.get('/login', async (req, res) => {
   try {
-    
+    res.render('login');
   } catch (err) {
     res.status(500).json(err);
   }
