1a864d90955104f58e668856e62b01aa6f2ee25c
Caitlin Wall <caitlinwall96@gmail.com>
2022-02-06 13:10:09 -0500

    Adjust app to server.listen in server.js and adjust chat files to fix working chat - no connection yet

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,25 @@
+var socket = io();
+    
+var messages = document.getElementById('chatmessages');
+var form = document.getElementById('chatform');
+var input = document.getElementById('chatinput');
+
+form.addEventListener('submit', function(e) {
+  e.preventDefault();
+  if (input.value) {
+    socket.emit('chat message', input.value);
+    input.value = '';
+  }
+});
+
+socket.on('chat message', function(msg) {
+  var item = document.createElement('li');
+  item.textContent = msg;
+  messages.appendChild(item);
+  window.scrollTo(0, document.body.scrollHeight);
+});
+
+
 // console.log('checks for io')
 // var socket = io();
 
@@ -20,35 +42,35 @@
 //   window.scrollTo(0, document.body.scrollHeight);
 // });
 
-console.log('doesnt break before reaching script')  
-let socket = io();
-  console.log('declares io')
-let messages = document.getElementById('chat-messages');
-  console.log('grabs chat-messages')
-let chatForm = document.getElementById('chat-form');
-  console.log('grabs chat-form')
-let input = document.getElementById('chat-input');
-  console.log('grabs chat-input');
+// console.log('doesnt break before reaching script')  
+// let socket = io();
+//   console.log('declares io')
+// let messages = document.getElementById('chat-messages');
+//   console.log('grabs chat-messages')
+// let chatForm = document.getElementById('chat-form');
+//   console.log('grabs chat-form')
+// let input = document.getElementById('chat-input');
+//   console.log('grabs chat-input');
   
+// // chatForm.addEventListener('submit', function(e) {
+// //   console.log('adds eventlistener to chat-form');
+// //   e.preventDefault();
+      
+// //   if (input.value) {
+// //     socket.emit('chat message', input.value);
+// //     input.value = '';
+// //   }
+// // });
+
 // chatForm.addEventListener('submit', function(e) {
 //   console.log('adds eventlistener to chat-form');
-//   e.preventDefault();
+//   // e.preventDefault();
       
-//   if (input.value) {
-//     socket.emit('chat message', input.value);
-//     input.value = '';
-//   }
+//   // if (input.value) {
+//   //   socket.emit('chat message', input.value);
+//   //   input.value = '';
+//   // }
 // });
-
-chatForm.addEventListener('submit', function(e) {
-  console.log('adds eventlistener to chat-form');
-  // e.preventDefault();
-      
-  // if (input.value) {
-  //   socket.emit('chat message', input.value);
-  //   input.value = '';
-  // }
-});
 
 // socket.on('chat message', function(msg) {
 //   var item = document.createElement('li');
