d12d10d612e8a84148504b0bfcbd1de63b0c2648
ealisa818 <ealisa818@gmail.com>
2022-02-07 12:46:56 -0500

    fix merge issues

--- controllers/api/profileRoutes.js
+++ controllers/api/profileRoutes.js
@@ -3,11 +3,10 @@ const { Profile } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 
-// route to create profile if user is logged in
+// route to create profile 
 router.post('/', async (req, res) => {
   try {
-    console.log("bye");
-    //create a profile using the req.body and set is_mentor to false to start
+      //create a profile using the req.body and set is_mentor to false to start
     const newProfile = await Profile.create({
       name: req.body.name,
       about: req.body.about,
@@ -24,21 +23,5 @@ router.post('/', async (req, res) => {
   }
 }
 );
-
-// //route to update profile 
-// router.put('/:id', withAuth, async (req, res) => {
-//   try {
-//     const profileData = await Profile.update({
-//       ...req.user_id,
-//       user_id: req.session.user_id,
-//     });
-//     res.status(200).json(profileData);
-//   } catch (err) {
-//     res.status(400).json(err)
-// //   }
-
-// });
-
-
 
 module.exports = router;
