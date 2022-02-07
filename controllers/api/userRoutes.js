4191f643ae3ff8dab3ae8e1c481a718954cf33dc
ealisa818 <ealisa818@gmail.com>
2022-02-07 12:29:33 -0500

    route user to profile form then dashboard

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -12,7 +12,7 @@ router.post('/', async (req, res) => {
     //session state is saved as being logged in
     req.session.save(() => {
       req.session.user_id = userData.id
-      // req.session.logged_in = true;
+      req.session.logged_in = true;
 
     res.status(200).json(userData); 
     });
