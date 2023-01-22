56fdffd855a3cc1108820c330c93bcbb06a5c38b
Caitlin Wall <caitlinwall96@gmail.com>
2023-01-22 16:32:31 -0500

    Adjust port

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,16 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
-//use JAWSDB for heroku deployment
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
+const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
       host: 'localhost',
       dialect: 'mysql',
-      port: 3306
+      PORT: process.env.PORT || 4000
     }
-  );
-}
+);
 
 module.exports = sequelize;
+ 
